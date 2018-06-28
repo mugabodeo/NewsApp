eba217bac544071066fa4b4e37ef4b4631d8f24f
Gaiish <gdishim@gmail.com>
2018-06-28 14:26:21 +0300

    adding comments in the code

--- App.js
+++ App.js
@@ -1,3 +1,37 @@
+/*
+  Hi there,
+  We'll filling the code with a lot of comments,
+  believe it will you guys understand the code more better
+*/
+
+/*
+  This small app just fetches some news data from a news api,
+  which is returned to us by the api in a JSON format(it's just an array
+  of objects: [{...}, {...}, {...}]) and renders to our UI via FlatList.
+  A FlatList is just a react native component that helps us render lists,
+  and has some cool features like refreshing the list if some more data
+  is coming in.
+  https://facebook.github.io/react-native/docs/flatlist.html
+  https://medium.com/react-native-development/how-to-use-the-flatlist-component-react-native-basics-92c482816fe6
+*/
+
+ /*
+  An API (application programming interface) you can think of it as a gate
+  to someones code allowing to perform some tasks using his code(depending
+  on permissions he gave you).
+  We all know google maps, this is an example of an API,
+  if you have your own app you can be able to add it in(the google maps api),
+  by that getting access to google servers and do some cool stuff with
+  maps...
+  https://medium.freecodecamp.org/what-is-an-api-in-english-please-b880a3214a82
+ */
+
+ /*
+  In our app we have mainly 2 components, a flatlist helping us,
+  to make a list of news article and an Article component
+  that describes how each article should look like
+ */
+
 import React from 'react';
 import { FlatList } from 'react-native';
 
--- getNews.js
+++ getNews.js
@@ -1,5 +1,6 @@
 
 export async function getNews() {
-  let result = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=675ffde3e15f42cc842c2d7c3279d5b5').then(response => response.json());
+  let result =
+    await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=675ffde3e15f42cc842c2d7c3279d5b5').then(response => response.json());
   return result.articles;
 }
